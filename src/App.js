37f178e48482a4c0b9e5b6a5cb5c08ac62cda5bf
Priyanka Jacob <priyanka.jacob93@gmail.com>
2023-05-12 00:58:22 +0530

    fix:Add HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 
 import Home from './components/Home';

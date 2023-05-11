3548b8c3aee770bb502e5f5c5f00e9a1d1de1b10
Priyanka Jacob <priyanka.jacob93@gmail.com>
2023-05-12 00:32:05 +0530

    fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,9 +2,7 @@ const Contact = () => {
     return(
         <>
           <h1>Finally, you are here! Let's catch up further!</h1>
-          <p>You can reach out to me via phone : &nbsp;
-              <a href="tel:+91-9886068885">+91-9886068885</a>&nbsp;
-              or mail me : <a href="mailto:priyanka.jacob93@gmail.com?subject=Hi, Priyanka" target="_blank" rel="noreferrer">priyanka.jacob93@gmail.com </a>
+          <p>You can reach me @ <a href="mailto:priyanka.jacob93@gmail.com?subject=Hi, Priyanka" target="_blank" rel="noreferrer">priyanka.jacob93@gmail.com </a>
           </p>
         </>
     )
